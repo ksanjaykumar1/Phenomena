a6d75a153f975dcf9c75ef7253bcd16af4392232
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-07 06:33:37 +0530

    added user sign up route

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.listen(3000,()=>{
-    console.log("The server is running on port 3000")
-})
+app.post('/api/1.0/users', (req, res) => {
+  return res.status(200).send();
+});
+
+module.exports = app;
