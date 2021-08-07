ed41c321777af39cdb6278ca6e01fc22afa3630d
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-07 11:53:24 +0530

    called synchronize function to sync all models

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const app = require('./app');
+const app = require('./src/app');
+const sequlize = require('./src/config/database');
+
+//You can use sequelize.sync() to automatically synchronize all models.
+sequlize.sync();
 
 app.listen(3000, () => {
   console.log('The server is listening at 3000....');
