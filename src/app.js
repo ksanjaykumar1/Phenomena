379dba918c750e9ca87f85b764ddf7ca79fc57a6
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-07 11:40:12 +0530

    added express json parser

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const User = require('./user/User');
 
+app.use(express.json());
 app.post('/api/1.0/users', (req, res) => {
-  return res.status(200).send({message:'User created'});
+  User.create(req.body).then(() => {
+    return res.status(200).send({ message: 'User created' });
+  });
 });
 
 module.exports = app;
