e83206a5d4c35bebbf9b02d35d85335e86e273f5
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-07 11:43:44 +0530

    added test to check if user details stored in the database

--- __test__/userRegister.spec.js
+++ __test__/userRegister.spec.js
@@ -1,33 +1,83 @@
+const { response } = require('express');
 const request = require('supertest');
-const app = require('../app');
+const app = require('../src/app');
+const sequelize = require('../src/config/database');
+const User = require('../src/user/User');
+const Sequelize = require('sequelize');
 
-it('it returns 200 when signup request is valid', (done) => {
-  request(app)
-    .post('/api/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@gamil.com',
-      password: 'P@assword',
-    })
-    .then((response) => {
-      expect(response.status).toBe(200);
-      done();
-    });
-
-  // if expect is not passed with done then it get value asynchronously i.e test doesn't wait for response
-  // we use expect inside then to handle the promise
+beforeAll(() => {
+  // run before each test suit
+  return sequelize.sync();
+});
+beforeEach(() => {
+  // cleaning the user table before each test
+  return User.destroy({ truncate: true });
 });
 
-it('returns success message when signup request is valid', (done) => {
-  request(app)
-    .post('/api/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@gamil.com',
-      password: 'P@assword',
-    })
-    .then((response) => {
-      expect(response.body.message).toBe('User created');
-      done();
-    });
+describe('User registration', () => {
+  it('it returns 200 when signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P@assword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+
+    // if expect is not passed with done then it get value asynchronously i.e test doesn't wait for response
+    // we use expect inside then to handle the promise
+  });
+
+  it('returns success message when signup request is valid', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gamil.com',
+        password: 'P@assword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
+
+  it('saves the user to the database', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P@ssword',
+      })
+      .then((response) => {
+        // query user table and then do the assertion based on it
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('saves the username and email to database ', (done) => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P@ssword',
+      })
+      .then((response) => {
+        User.findAll().then((userList) => {
+          const savedUser = userList[0];
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@gmail.com');
+          done();
+        });
+      });
+  });
 });
