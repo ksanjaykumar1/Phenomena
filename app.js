52ee1f27dd78c782a6c3d8500a8097b12ed8b192
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-07 07:38:47 +0530

    sign up request response message added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.post('/api/1.0/users', (req, res) => {
-  return res.status(200).send();
+  return res.status(200).send({message:'User created'});
 });
 
 module.exports = app;
