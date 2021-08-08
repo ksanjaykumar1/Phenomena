319dbb27057a0fda149ca4e936fc68382069fcf3
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 14:32:15 +0530

    test for username validation added

--- __test__/userRegister.spec.js
+++ __test__/userRegister.spec.js
@@ -11,39 +11,43 @@ beforeEach(() => {
   // cleaning the user table before each test
   return User.destroy({ truncate: true });
 });
-
+const validUser = {
+  username: 'user1',
+  email: 'user1@gmail.com',
+  password: 'P@assword',
+};
+const invalidUser = {
+  username: null,
+  email: 'user1@gmail.com',
+  password: 'P@ssword',
+};
+// function to call post api to users
+const postUser = (user = validUser) => {
+  return request(app).post('/api/1.0/users').send(user);
+};
 describe('User registration', () => {
-  // function to call post api to users
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username: 'user1',
-      email: 'user1@gmail.com',
-      password: 'P@assword',
-    });
-  };
-
   it('it returns 200 when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.status).toBe(200);
 
     // if expect is not passed with done then it get value asynchronously i.e test doesn't wait for response
     // we use expect inside then to handle the promise
   });
 
-  it('returns success message when signup request is valid', async() => {
-    const response = await postValidUser();
+  it('returns success message when signup request is valid', async () => {
+    const response = await postUser();
     expect(response.body.message).toBe('User created');
   });
 
-  it('saves the user to the database', async() => {
-    await postValidUser();
+  it('saves the user to the database', async () => {
+    await postUser();
     // query user table and then do the assertion based on it
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
 
-  it('saves the username and email to database ', async() => {
-    await postValidUser();
+  it('saves the username and email to database ', async () => {
+    await postUser();
     // query user table and then do the assertion based on it
     const userList = await User.findAll();
     const savedUser = userList[0];
@@ -51,10 +55,25 @@ describe('User registration', () => {
     expect(savedUser.email).toBe('user1@gmail.com');
   });
 
-  it('hashes the password in the database', async() => {
-    await postValidUser();
-    const userList = await User.findAll()
-    const SavedUser = userList[0]
-    expect(SavedUser.password).not.toBe('P@ssword')
-  })
+  it('hashes the password in the database', async () => {
+    await postUser();
+    const userList = await User.findAll();
+    const SavedUser = userList[0];
+    expect(SavedUser.password).not.toBe('P@ssword');
+  });
+
+  it('returns 400 when usernme is null', async () => {
+    const response = await postUser(invalidUser);
+    expect(response.status).toBe(400);
+  });
+
+  it('return validationErrors field in response body when validation error occurs ',async () => {
+    const response = await postUser(invalidUser);
+    expect(response.body.validationErrors).not.toBeUndefined();
+  });
+
+  it('return username cannot be null when username is null ',async () => {
+    const response = await postUser(invalidUser);
+    expect(response.body.validationErrors.username).toBe('Username cannot be null');
+  });
 });
