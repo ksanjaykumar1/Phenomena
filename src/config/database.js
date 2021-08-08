d0b50f7273116aad43e264d1a8c8b446f067184e
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 14:31:01 +0530

    added capability to support different envirnoments

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,15 @@
 const Sequelize = require('sequelize');
+const config = require('config');
+// to get the configuration from the config folder at the start
+// in package.json we wrote a script which set the node envirnonment
+// For development , NODE_ENV is set to development which passed to index which in turn passes to app, then config selects the development.json from the config folder
+// For test, NODE_ENV is set test 
+const dbConfig = config.get('database');
+
 // creating our sequlize instance
-const sequelize = new Sequelize('hoaxify', 'my-db-user', 'db-pass', {
-  dialect: 'sqlite',
-  storage: './database.sqlite',
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  storage: dbConfig.storage,
   logging: false,
 });
 
