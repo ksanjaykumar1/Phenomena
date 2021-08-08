a3c73d4c1a98be17ac5fce515ad7b93324dd3064
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 07:11:56 +0530

    moved user route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const User = require('./user/User');
+const users = require('./routes/users')
+
 
 app.use(express.json());
-app.post('/api/1.0/users', (req, res) => {
-  User.create(req.body).then(() => {
-    return res.status(200).send({ message: 'User created' });
-  });
-});
+app.use('/api/1.0/users',users)
 
 module.exports = app;
