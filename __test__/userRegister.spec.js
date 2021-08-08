866f26973452529bf9f0054cb20f331d36955e81
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 08:07:09 +0530

    changed to async/await to handle promise

--- __test__/userRegister.spec.js
+++ __test__/userRegister.spec.js
@@ -22,51 +22,39 @@ describe('User registration', () => {
     });
   };
 
-  it('it returns 200 when signup request is valid', (done) => {
-    postValidUser().then((response) => {
-      expect(response.status).toBe(200);
-      done();
-    });
+  it('it returns 200 when signup request is valid', async () => {
+    const response = await postValidUser();
+    expect(response.status).toBe(200);
 
     // if expect is not passed with done then it get value asynchronously i.e test doesn't wait for response
     // we use expect inside then to handle the promise
   });
 
-  it('returns success message when signup request is valid', (done) => {
-    postValidUser().then((response) => {
-      expect(response.body.message).toBe('User created');
-      done();
-    });
+  it('returns success message when signup request is valid', async() => {
+    const response = await postValidUser();
+    expect(response.body.message).toBe('User created');
   });
 
-  it('saves the user to the database', (done) => {
-    postValidUser().then(() => {
-      // query user table and then do the assertion based on it
-      User.findAll().then((userList) => {
-        expect(userList.length).toBe(1);
-        done();
-      });
-    });
+  it('saves the user to the database', async() => {
+    await postValidUser();
+    // query user table and then do the assertion based on it
+    const userList = await User.findAll();
+    expect(userList.length).toBe(1);
   });
 
-  it('saves the username and email to database ', (done) => {
-    postValidUser().then(() => {
-      User.findAll().then((userList) => {
-        const savedUser = userList[0];
-        expect(savedUser.username).toBe('user1');
-        expect(savedUser.email).toBe('user1@gmail.com');
-        done();
-      });
-    });
+  it('saves the username and email to database ', async() => {
+    await postValidUser();
+    // query user table and then do the assertion based on it
+    const userList = await User.findAll();
+    const savedUser = userList[0];
+    expect(savedUser.username).toBe('user1');
+    expect(savedUser.email).toBe('user1@gmail.com');
   });
 
-  it('hashes the password in the database', (done) => {
-    postValidUser().then(() => {
-      User.findAll().then((userList) => {
-        const savedUser = userList[0];
-        expect(savedUser.password).not.toBe('P@ssword');
-        done();
-      });
-    });
-  });
+  it('hashes the password in the database', async() => {
+    await postValidUser();
+    const userList = await User.findAll()
+    const SavedUser = userList[0]
+    expect(SavedUser.password).not.toBe('P@ssword')
+  })
 });
