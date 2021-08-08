f2effef3ccbf36560f1f78eb50d127f746bc7af4
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 07:25:10 +0530

    refactored the register function to a controller

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
+const {register} = require('../controller/users')
 
-router.post('/', async (req, res) => {
-  const hash = await bcrypt.hash(req.body.password, 10);
-  const user = { ...req.body, password: hash };
-  await User.create(user);
-  return res.status(200).send({ message: 'User created' });
-});
+router.post('/',register);
 
 module.exports = router;
