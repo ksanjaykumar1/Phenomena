02cfd5dc2dd772bf8bf162bdf4cb054dcd89aa55
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 06:37:30 +0530

    moved to models folder

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.post('/api/1.0/users', (req, res) => {
-  return res.status(200).send({message:'User created'});
-});
-
-module.exports = app;
