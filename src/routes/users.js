841668f4e413f47655d17beec5f336b89f2c205d
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-08 14:31:40 +0530

    user validation added

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const {register} = require('../controller/users')
+const { register } = require('../controller/users');
 
-router.post('/',register);
+const validateUsername = (req, res, next) => {
+  const user = req.body;
+  if (user.username === null) {
+    return res.status(400).send({
+      validationErrors: {
+        username: 'Username cannot be null',
+      },
+    });
+  }
+  next();
+};
+
+router.post('/', validateUsername, register);
 
 module.exports = router;
