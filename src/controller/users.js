7785f84a74f455b40314fd89e98fbf51760db629
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-09 16:14:13 +0530

    added findUserByEmail and to handle proper registration with inactive column

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,13 +1,26 @@
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
-const register =async (req, res) => {
-    const hash = await bcrypt.hash(req.body.password, 10);
-    const user = { ...req.body, password: hash };
-    await User.create(user);
-    return res.status(200).send({ message: 'User created' });
+const register = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const validationErrors = {};
+    errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+    return res.status(400).send({ validationErrors: validationErrors });
   }
+  const { username, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = { username, email, password: hash };
+  await User.create(user);
+  return res.status(200).send({ message: 'User created' });
+};
+
+const findUserByEmail = (email) => {
+  return User.findOne({ where: { email: email } });
+};
 
 module.exports = {
-    register
-}
+  register,
+  findUserByEmail,
+};
