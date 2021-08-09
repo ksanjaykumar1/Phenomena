59ae5de5f870482b8cd87c3b612d96005ffee8e9
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-09 16:17:26 +0530

    error handler to prevent same email id to register again

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controller/users');
+const { register, findUserByEmail } = require('../controller/users');
 const { check, validationResult } = require('express-validator');
+const User = require('../model/User');
 
 router.post(
   '/',
-  check('username').notEmpty().withMessage('Username cannot be null'),
-  check('email').notEmpty().withMessage('Email cannot be null'),
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 37 characters'),
+  check('email')
+    .notEmpty()
+    .withMessage('Email cannot be null')
+    .bail()
+    .isEmail()
+    .withMessage('E-mail is is not vaild')
+    .bail()
+    .custom(async (email) => {
+      const user = await findUserByEmail(email);
+      if (user) {
+        throw new Error('E-mail in use');
+      }
+    }),
+  check('password')
+    .notEmpty()
+    .withMessage('Password cannot be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters')
+    .bail()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
+    .withMessage('Password must have at least 1 uppercare 1 lowercase 1 number 1 special character'),
   register
 );
 
