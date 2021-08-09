da24edcf75627e81d1273f83380998b969142ec4
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-09 16:14:59 +0530

    added 2 tests to handle inactive column

--- __test__/userRegister.spec.js
+++ __test__/userRegister.spec.js
@@ -14,12 +14,12 @@ beforeEach(() => {
 const validUser = {
   username: 'user1',
   email: 'user1@gmail.com',
-  password: 'P@assword',
+  password: 'P4@assword',
 };
 const invalidUser = {
   username: null,
   email: 'user1@gmail.com',
-  password: 'P@ssword',
+  password: 'P4@ssword',
 };
 // function to call post api to users
 const postUser = (user = validUser) => {
@@ -59,7 +59,7 @@ describe('User registration', () => {
     await postUser();
     const userList = await User.findAll();
     const SavedUser = userList[0];
-    expect(SavedUser.password).not.toBe('P@ssword');
+    expect(SavedUser.password).not.toBe('P4ssword');
   });
 
   it('returns 400 when usernme is null', async () => {
@@ -76,7 +76,7 @@ describe('User registration', () => {
     const response = await postUser({
       username: null,
       email: null,
-      password: 'P@assword',
+      password: 'P4@assword',
     });
     expect(Object.keys(response.body.validationErrors)).toEqual(['username', 'email']);
   });
@@ -84,12 +84,21 @@ describe('User registration', () => {
   //dynamic tests
 
   it.each`
-    field         | value   | expectedMessage
-    ${'username'} | ${null} | ${'Username cannot be null'}
-    ${'email'}    | ${null} | ${'Email cannot be null'}
-    ${'password'} | ${null} | ${'Password cannot be null'}
-    ${'username'} | ${'use'} | ${'Must have min 4 and max 37 characters'}
-  `(`returns $expectedMessage when $field is null`, async ({ field, expectedMessage,value }) => {
+    field         | value             | expectedMessage
+    ${'username'} | ${null}           | ${'Username cannot be null'}
+    ${'username'} | ${'use'}          | ${'Must have min 4 and max 37 characters'}
+    ${'username'} | ${'a'.repeat(37)} | ${'Must have min 4 and max 37 characters'}
+    ${'email'}    | ${null}           | ${'Email cannot be null'}
+    ${'email'}    | ${'mail.com'}     | ${'E-mail is is not vaild'}
+    ${'email'}    | ${'user'}         | ${'E-mail is is not vaild'}
+    ${'email'}    | ${'user@gmail'}   | ${'E-mail is is not vaild'}
+    ${'email'}    | ${'user.com.com'} | ${'E-mail is is not vaild'}
+    ${'password'} | ${null}           | ${'Password cannot be null'}
+    ${'password'} | ${'p4ss'}         | ${'Password must be at least 6 characters'}
+    ${'password'} | ${'alllowercase'} | ${'Password must have at least 1 uppercare 1 lowercase 1 number 1 special character'}
+    ${'password'} | ${'ALLUPERCASE'}  | ${'Password must have at least 1 uppercare 1 lowercase 1 number 1 special character'}
+    ${'password'} | ${'4444444444'}   | ${'Password must have at least 1 uppercare 1 lowercase 1 number 1 special character'}
+  `(`returns $expectedMessage when $field is null`, async ({ field, expectedMessage, value }) => {
     const user = {
       username: 'user1',
       email: 'user1@gmail.com',
@@ -100,13 +109,42 @@ describe('User registration', () => {
     expect(response.body.validationErrors[field]).toBe(expectedMessage);
   });
 
-  it('returns size validation error when username is less than 4 character', async () => {
-    const user = {
-      username: 'use',
+  it('returns E-mail in use when same email is already in use', async () => {
+    // maunally add
+    await User.create({ ...validUser });
+    const response = await postUser(validUser);
+    expect(response.body.validationErrors.email).toBe('E-mail in use');
+  });
+
+  it('returns error for both usernme is null and email in use', async () => {
+    // maunally add
+    await User.create({ ...validUser });
+    const response = await postUser({
+      username: null,
       email: 'user1@gmail.com',
-      password: 'password',
+      password: 'P4@ssword',
+    });
+    expect(Object.keys(response.body.validationErrors)).toEqual(['username', 'email']);
+  });
+
+  it('creates a user in a inactive mode', async () => {
+    await postUser();
+    const users = await User.findAll();
+    const savedUser = users[0];
+    expect(savedUser.inactive).toBe(true);
+  });
+
+  it('creates a user in inactive mode even when the request body contains inactive as false', async () => {
+    const newUser = {
+      username: 'user1',
+      email: 'user1@gmail.com',
+      password: 'P4@ssword',
+      inactive: false,
     };
-    const response = await postUser(user);
-    expect(response.body.validationErrors.username).toBe('Must have min 4 and max 37 characters');
+    await postUser(newUser);
+    const users = await User.findAll();
+    const savedUser = users[0];
+    console.log(savedUser);
+    expect(savedUser.inactive).toBe(true);
   });
 });
