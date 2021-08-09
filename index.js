bd5dc3075a11c6ea7ff9b8b0b03f8a179719c812
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-09 16:12:08 +0530

    database is deleted everytime the app restarts

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const app = require('./src/app');
 const sequlize = require('./src/config/database');
 
 //You can use sequelize.sync() to automatically synchronize all models.
-sequlize.sync();
-
+//{force:true} deletes the database everytime the app restarts after changes 
+sequlize.sync({force:true});
 app.listen(3000, () => {
   console.log('The server is listening at 3000....');
 });
