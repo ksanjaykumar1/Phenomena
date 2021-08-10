3dd9718e7f35aacedd722467dadfe4ff48cd2dcb
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-10 23:42:54 +0530

    generating and sending activation token to email

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,7 +1,12 @@
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const { validationResult } = require('express-validator');
+const crypto = require('crypto')
+const {sendAccountActivation} =require('./email')
 
+const generateToken =(length)=>{
+    return crypto.randomBytes(length).toString('hex').substring(0,length)
+}
 const register = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -11,7 +16,8 @@ const register = async (req, res) => {
   }
   const { username, email, password } = req.body;
   const hash = await bcrypt.hash(password, 10);
-  const user = { username, email, password: hash };
+  const user = { username, email, password: hash,activationToken: generateToken(16) };
+  await sendAccountActivation(email,user.activationToken)
   await User.create(user);
   return res.status(200).send({ message: 'User created' });
 };
