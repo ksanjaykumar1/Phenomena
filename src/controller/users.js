19af851881abb4f09fb275dca6da024dfef35c85
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-08-11 06:39:46 +0530

    added capabalility to handle email faliure
    Signed-off-by: ksanjaykumar1 <ksanjayk@protonmail.com>

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,25 +1,39 @@
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const { validationResult } = require('express-validator');
-const crypto = require('crypto')
-const {sendAccountActivation} =require('./email')
+const crypto = require('crypto');
+const Email = require('./email');
+const sequelize = require('../config/database');
 
-const generateToken =(length)=>{
-    return crypto.randomBytes(length).toString('hex').substring(0,length)
-}
+const generateToken = (length) => {
+  return crypto.randomBytes(length).toString('hex').substring(0, length);
+};
 const register = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const validationErrors = {};
-    errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
-    return res.status(400).send({ validationErrors: validationErrors });
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
+    const { username, email, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const user = { username, email, password: hash, activationToken: generateToken(16) };
+    const transaction = await sequelize.transaction();
+    // returns the transaction
+    await User.create(user, { transaction });
+
+    try {
+      await Email.sendAccountActivation(email, user.activationToken);
+      await transaction.commit();
+    } catch (err) {
+      await transaction.rollback();
+      throw new Error(err);
+    }
+    return res.status(200).send({ message: 'User created' });
+  } catch (err) {
+    return res.status(502).send({ message: 'E-mail Failure' });
   }
-  const { username, email, password } = req.body;
-  const hash = await bcrypt.hash(password, 10);
-  const user = { username, email, password: hash,activationToken: generateToken(16) };
-  await sendAccountActivation(email,user.activationToken)
-  await User.create(user);
-  return res.status(200).send({ message: 'User created' });
 };
 
 const findUserByEmail = (email) => {
